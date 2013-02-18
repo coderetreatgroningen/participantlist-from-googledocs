4e455d68d79596a8043f80d5d861994b4bfbe1a2
Mark IJbema <markijbema@gmail.com>
2013-02-18 21:48:29 +0100

    put out a list of names for users who don't have avatars

--- fetch.rb
+++ fetch.rb
@@ -2,32 +2,47 @@ require "rubygems"
 require "google_drive"
 require 'gravatar-ultimate'
 
-username = ARGV[0]
+$username = ARGV[0]
 password = ARGV[1]
+$gravatar_password = password + '123'
 
 
 name_column = 'B'.ord - 65 + 1
 email_column = 'H'.ord - 65 + 1
 
 
+def has_gravatar(email)
+	api = Gravatar.new($username, password: $gravatar_password)
+	api.exists?(email)
+end
+
 # Logs in.
 # You can also use OAuth. See document of
 # GoogleDrive.login_with_oauth for details.
-session = GoogleDrive.login(username, password)
+session = GoogleDrive.login($username, password)
 
 # First worksheet of
 # https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
 ws = session.spreadsheet_by_key("0AsxYGKsq3C89dC1velY1SFpWbl9uRUZ6Q05jVjRJZEE").worksheets[0]
 
+pictures = ''
+names = []
 # Dumps all cells.
 for row in 2..ws.num_rows
   name = ws[row, name_column]
   encoded_name = name.gsub /[^a-zA-Z0-9' ]/, ''
 
   email = ws[row, email_column]
-  gravatar = Gravatar.new(email).image_url(default: :monsterid, size: 150)
 
-  puts "<li><img src=\"#{gravatar}\" alt=\"#{encoded_name}\" title=\"#{encoded_name}\"></li>"
+  if has_gravatar(email)
+  	gravatar = Gravatar.new(email).image_url(default: :monsterid, size: 150)
+  	pictures << "<li><img src=\"#{gravatar}\" alt=\"#{encoded_name}\" title=\"#{encoded_name}\"></li>"
+  else
+  	names << encoded_name
+  end
 end
 
+puts pictures
+
+puts names.join(', ')
 
